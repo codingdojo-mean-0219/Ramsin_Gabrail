a99dc9d10e52d29ac6c578cdf2d0e072f3db8c8b
Ramsin Gabrail <ramsin.gabrail@gmail.com>
2019-04-05 22:48:57 -0700

    Updating Restful Tasks Ctd.

--- 05-angular/03-dom-manipulation/Restful_Tasks_Continued/public/src/app/app.component.ts
+++ 05-angular/03-dom-manipulation/Restful_Tasks_Continued/public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,10 +6,32 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angularizing';
+export class AppComponent implements OnInit {
+  title = 'Restful Tasks Continued';
+  tasks: any
+  showing: any
+  showAll: any
 
-  constructor(private _httpService: HttpService) {
+  showTask(obj: object): void {
+    console.log(`Displaying tasks: ${obj}`)
+    this.showing =obj
+    ;}
 
+  constructor(private _httpService: HttpService){}
+  ngOnInit() {
   }
+  onshowAll() {
+     console.log("showing all tasks");
+    this.getTasksFromService()
+   }
+  getTasksFromService(){
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data => {
+       this.tasks = data;
+       console.log(data);
+       console.log(this.tasks," These are the tasks");
+       this.showAll = true
+    });
+  }
+
 }
